ced052a21926a76d8371653d53bec06cabe55028
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-09-14 22:07:16 -0300

    js

---
+++ Funcoes/funcoesAnonimas.js
@@ -0,0 +1,23 @@
+// função anônima: funçaõs sem nome
+
+const soma = function(x,y){
+return x + y
+}
+
+const imprimirResultado= function(a,b, operacao = soma){
+    console.log(operacao(a,b))
+}
+
+imprimirResultado(3,4)
+imprimirResultado(3,4,soma) // chamando explicitamente a funcao soma
+imprimirResultado(3,4, function(x,y){ //chamando outra funcao
+    return x-y
+})
+imprimirResultado(3,4,(x,y) => x * y) //arrow function
+
+const pessoa= {
+    falar: function(){
+        console.log('Opa')
+    }
+}
+pessoa.falar()

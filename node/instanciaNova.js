67a6d2ca2db3d0cb90177891230e8b54c5e8a9ec
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-09-29 22:22:49 -0300

    js

---
+++ node/instanciaNova.js
@@ -0,0 +1,10 @@
+//Um factory retorna um novo objeto
+
+module.exports = () => {
+    return {
+        valor: 1,
+        inc(){
+            this.valor++
+        }
+    }
+}
---
+++ node/instanciaUnica.js
@@ -0,0 +1,7 @@
+//node faz cache = unica instancia
+module.exports = {
+    valor: 1,
+    inc(){
+        this.valor++
+    }
+}

757fc3d78f8a0ccdc3cceb4503ac5c62be5e692a
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-10-09 22:12:12 -0300

    js

--- node/temporizador.js
+++ node/temporizador.js
@@ -7,3 +7,14 @@ const tarefa1 = shedule.scheduleJob('*/5 * 12 ** 2', function (){
 setTimeout(function (){
     tarefa1.cancel()
 },20000)
+
+const regra = new shedule.RecurrenceRule()
+
+regra.dayOfWeek = [new shedule.Range(1,5)]
+regra.hour = 12
+regra.second = 30
+
+
+const tarefa2 = shedule.scheduleJob('*/5 * 12 ** 2', function (){
+    console.log('Executando tarefa 1', new Date().getSeconds)
+})

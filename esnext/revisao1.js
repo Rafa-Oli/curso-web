28ebc0065aef6c5ac4fd1e2c88e3e38baf9f5095
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-10-10 18:55:38 -0300

    js

--- esnext/revisao1.js
+++ esnext/revisao1.js
@@ -6,3 +6,30 @@ function total(...numeros){
 }
 
 console.log(total(2,3,4,5))
+
+
+//EC8: Object.values/Object.entries
+const obj = {a: 1,b: 2, c: 3}
+console.log(Object.values(obj))
+console.log(Object.entries(obj))
+
+
+//Melhorias na Notação Literal
+const nome = 'Carla'
+const pessoa = {
+    nome,
+    ola(){
+        return 'Oi gente'
+    }
+}
+console.log(pessoa.nome,pessoa.ola())
+
+//class
+class Animal{}
+class Cachorro extends Animal{
+    falar(){
+        return 'AU AU'
+    }
+}
+
+console.log(new Cachorro().falar())
